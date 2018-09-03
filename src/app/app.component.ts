79e0c6e9f86f17556bd507393d444ffd4ace9e8f
paul.thomas@neutrinos.co <paul.thomas@neutrinos.co>
2018-09-03 09:18:58 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'chatbot';
+export class AppComponent implements OnInit {
+  para = {
+    'Latitude': 0,
+    'Longitude': 0,
+    'Altitude': 0,
+    'Accuracy': 0,
+    'Heading': 0,
+    'Speed': 0,
+    'Timestamp': 0
+  };
+
+  ngOnInit(): void {
+    // document.addEventListener('deviceready', this.onDeviceReady, false);
+    this.onDeviceReady();
+
+  }
+
+  onDeviceReady() {
+    console.log('navigator.geolocation works well');
+    navigator.geolocation.watchPosition((position) => {
+      console.log(this);
+      this.para.Latitude =  position.coords.latitude;
+      this.para.Longitude =  position.coords.longitude;
+      this.para.Altitude =  position.coords.altitude;
+      this.para.Accuracy = position.coords.accuracy;
+      this.para.Heading =  position.coords.heading;
+      this.para.Speed = position.coords.speed;
+      this.para.Timestamp =  position.timestamp;
+    },
+      this.onError,
+      { timeout: 30000, enableHighAccuracy: true });
+  }
+
+  // onSuccess Callback
+  // This method accepts a Position object, which contains the
+  // current GPS coordinates
+  //
+  onSuccess(position) {
+    console.log(this);
+    this.para.Latitude =  position.coords.latitude;
+    this.para.Longitude =  position.coords.longitude;
+    this.para.Altitude =  position.coords.altitude;
+    this.para.Accuracy = position.coords.accuracy;
+    this.para.Heading =  position.coords.heading;
+    this.para.Speed = position.coords.speed;
+    this.para.Timestamp =  position.timestamp;
+  }
+
+  // onError Callback receives a PositionError object
+  //
+  onError(error) {
+    alert('code: ' + error.code + '\n' +
+      'message: ' + error.message + '\n');
+  }
 }
